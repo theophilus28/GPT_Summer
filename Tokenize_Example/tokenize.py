96bcf72c78b46bea8ef75f5e9d4a6130432a1c5e
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-06-03 14:42:53 -0400

    decoder implemented, error flag accounted for

--- Tokenize_Example/tokenize.py
+++ Tokenize_Example/tokenize.py
@@ -66,4 +66,9 @@ vocab = {idx:  bytes([idx]) for idx in range(256)}
 for(p0, p1), idx in merges.items():
     vocab[idx] = vocab[p0] + vocab[p1]
 def decode(ids):
-    
+    #given ids (list of ints), return python string
+    tokens = b"".join(vocab[idx] for idx in ids)
+    text = tokens.decode("utf-8", errors="replace")
+    return text
+
+print(decode([128]))
