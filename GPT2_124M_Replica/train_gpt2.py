205bb873aa4963f6e93a41ae994dfd3a27c0f6f1
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-06-13 18:08:47 -0400

    CSA implemented, base gpt2 done

--- GPT2_124M_Replica/train_gpt2.py
+++ GPT2_124M_Replica/train_gpt2.py
@@ -1,9 +1,41 @@
+
+import math
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+class CasualSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        #key, query, value projections for all heads, but in batch
+        self.c_attn - nn.Linear(config.n_embed, 3 * config.n_embed)
+        #output projection
+        self.c_proj - nn.Linear(config.n_embed, config.n_embed)
+        #regularization
+        self.n_head = config.n_embed
+        self.n_embed = config.n_embed
+        #not really 'bias', more of a mask, but following OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
 
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embed, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2) #(B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) #(B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) #(B, nh, T, hs)
+        #attention (materializes the large (T,T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v #(B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.tranpose(1, 2).contiguous().view(B, T, C) # reassemble all head outputs side by side
+        #output projection
+        y = self.c_proj(y)
+        return y
+    
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__(config.n_embed, config.n_embed * 4)
@@ -16,8 +48,6 @@ class MLP(nn.Module):
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
-
-
 
 class Block(nn.Module):
     def __init__(self, config):
