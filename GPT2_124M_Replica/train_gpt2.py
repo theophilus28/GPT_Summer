a1c22b9f4fe771f7e1a939313556c4624d26bb5b
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-06-13 18:24:56 -0400

    added from_pretrained using huggingface

--- GPT2_124M_Replica/train_gpt2.py
+++ GPT2_124M_Replica/train_gpt2.py
@@ -1,4 +1,5 @@
-
+#Theo Barrett-Johnson 6/13/2024
+#this code is made with the help of a video, and im going through this for learning purposes
 import math
 from dataclasses import dataclass
 import torch
@@ -10,9 +11,9 @@ class CasualSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embed % config.n_head == 0
         #key, query, value projections for all heads, but in batch
-        self.c_attn - nn.Linear(config.n_embed, 3 * config.n_embed)
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
         #output projection
-        self.c_proj - nn.Linear(config.n_embed, config.n_embed)
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
         #regularization
         self.n_head = config.n_embed
         self.n_embed = config.n_embed
@@ -38,8 +39,8 @@ class CasualSelfAttention(nn.Module):
     
 class MLP(nn.Module):
     def __init__(self, config):
-        super().__init__(config.n_embed, config.n_embed * 4)
-        self.c_fc = nn.Linear()
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, config.n_embed * 4)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
 
@@ -52,24 +53,24 @@ class MLP(nn.Module):
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.ln1 = nn.LayerNorm(config.n_embed)
+        self.ln_1 = nn.LayerNorm(config.n_embed)
         self.attn = CasualSelfAttention(config)
-        self.ln2 = nn.LayerNorm(config.n_embed)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x +self.attn(self.ln1(x))
-        x = x +self.mlp(self.ln2(x))
+        x = x +self.attn(self.ln_1(x))
+        x = x +self.mlp(self.ln_2(x))
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embed: int = 384
+    block_size: int = 1024 #max sequence length
+    vocab_size: int = 50267 #number of tokens: 50,000 merges, 256 byte tokens, 1 <|endoftext|> token
+    n_layer: int = 12 #number of heads
+    n_head: int = 12 #number of layers
+    n_embed: int = 768 #embedding dimension
 
-class GPT(nn.module):
+class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -81,3 +82,56 @@ class GPT(nn.module):
             ln_f = nn.LayerNorm(config.n_embed)
         ))
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    #this next class is copy and pasted in from repo
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embed are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embed=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embed=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embed=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embed=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+#------------------------------------------------------------------------------\
+model = GPT.from_pretrained('gpt2')
+print("Epic! We didn't crash!")
