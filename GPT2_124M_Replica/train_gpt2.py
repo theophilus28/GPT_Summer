4f9e93b5ccd5e887b4f7ec02eebf24ed3e48b67c
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-06-13 17:39:07 -0400

    block implemented, no MLP or CSA

--- GPT2_124M_Replica/train_gpt2.py
+++ GPT2_124M_Replica/train_gpt2.py
@@ -4,6 +4,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embed)
+        self.attn = CasualSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x +self.attn(self.ln1(x))
+        x = x +self.mlp(self.ln2(x))
+
 @dataclass
 class GPTConfig:
     block_size: int = 256
