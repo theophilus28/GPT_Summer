36fbcd020a8d807dacca1a57498d45ec405328d5
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-06-20 13:14:15 -0400

    added sampler and optimizer

--- GPT2_124M_Replica/train_gpt2.py
+++ GPT2_124M_Replica/train_gpt2.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
 
 class CasualSelfAttention(nn.Module):
     def __init__(self, config):
@@ -154,6 +155,34 @@ class GPT(nn.Module):
 
         return model
 #------------------------------------------------------------------------------
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        #at init load tokes from disk and store them to memory
+        with open('../Starter_Example/input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        #state
+        self.current_position = 0
+        
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1].view(B,T)) #inputs
+        y = (buf[1:].view(B,T)) #targets
+        self.current_position += B * T
+        if self.current_position + (B*T+1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+#------------------------------------------------------------------------------
 #autodetect computing device
 device = "cpu"
 if torch.cuda.is_available():
@@ -161,24 +190,25 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
-device = "cpu" # OVERRIDE
 
-# get a data batch
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('../Starter_Example/input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+#replaces the get data batch segment
+train_loader = DataLoaderLite(B=4, T=32)
 
 # get logits
 model = GPT(GPTConfig())
 model.to(device)
-logits, loss = model(x, y)
+
+#optimization
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
 
 print(loss)
 import sys; sys.exit(0)
